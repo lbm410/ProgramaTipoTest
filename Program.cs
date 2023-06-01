c2e3fefabdc3b8a597491303a3777ed421225ad5
Lucas Barrientos Muñoz <lbm40@inlumine.ual.es>
2023-06-01 20:04:22 +0200

    Último cambio, API ChatGPT

--- Program.cs
+++ Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 class Program
 {
     static List<Pregunta> preguntas = new List<Pregunta>();
+    static ChatClient chatClient = new ChatClient();
 
     static void Main(string[] args)
     {
         // Cargar preguntas desde el archivo
+        CargarPreguntasDesdeArchivo();
+
+        bool salir = false;
+
+        while (!salir)
+        {
+            Console.WriteLine("==== Menú ====");
+            Console.WriteLine("1. Hacer tipo test");
+            Console.WriteLine("2. Introducir nueva pregunta tipo test");
+            Console.WriteLine("3. Eliminar pregunta");
+            Console.WriteLine("4. Listado de preguntas");
+            Console.WriteLine("5. Preguntar a ChatGPT");
+            Console.WriteLine("6. Salir");
+            Console.Write("Seleccione una opción: ");
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    HacerTipoTest();
+                    break;
+                case "2":
+                    IntroducirNuevaPregunta();
+                    break;
+                case "3":
+                    EliminarPregunta();
+                    break;
+                case "4":
+                    MostrarListadoPreguntas();
+                    break;
+                case "5":
+                    PreguntarAChatGPT();
+                    break;
+                case "6":
+                    salir = true;
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void CargarPreguntasDesdeArchivo()
+    {
         if (File.Exists("preguntas.txt"))
         {
             using (StreamReader sr = new StreamReader("preguntas.txt"))
@@ -35,47 +85,11 @@ class Program
                 }
             }
         }
-
-        bool salir = false;
-
-        while (!salir)
-        {
-            Console.WriteLine("==== Menú ====");
-            Console.WriteLine("1. Hacer tipo test");
-            Console.WriteLine("2. Introducir nueva pregunta tipo test");
-            Console.WriteLine("3. Eliminar pregunta");
-            Console.WriteLine("4. Listado de preguntas");
-            Console.WriteLine("5. Salir");
-            Console.Write("Seleccione una opción: ");
-            string opcion = Console.ReadLine();
-
-            switch (opcion)
-            {
-                case "1":
-                    HacerTipoTest();
-                    break;
-                case "2":
-                    IntroducirNuevaPregunta();
-                    break;
-                case "3":
-                    EliminarPregunta();
-                    break;
-                case "4":
-                    MostrarListadoPreguntas();
-                    break;
-                case "5":
-                    salir = true;
-                    break;
-                default:
-                    Console.WriteLine("Opción no válida. Intente de nuevo.");
-                    break;
-            }
-
-            Console.WriteLine();
-        }
     }
+
     static void HacerTipoTest()
     {
+        int puntuacion = 0;
         if (preguntas.Count == 0)
         {
             Console.WriteLine("No hay preguntas disponibles. Introduzca preguntas antes de realizar el tipo test.");
@@ -121,32 +135,35 @@ class Program
             // Leer respuesta del usuario
             Console.Write("Introduzca su respuesta: ");
             string respuestaUsuario = Console.ReadLine();
-
             // Verificar respuesta
             if (pregunta.Opciones[int.Parse(respuestaUsuario) - 1] == pregunta.RespuestaCorrecta)
             {
                 Console.WriteLine("¡Respuesta correcta!" + "\n");
+                puntuacion++;
             }
             else
             {
                 Console.WriteLine($"Respuesta incorrecta. La respuesta correcta es: {pregunta.RespuestaCorrecta}" + "\n");
+                puntuacion--;
             }
 
             preguntasMostradas++;
             indicePregunta++;
         }
 
+        double notaFinal = (double)puntuacion / preguntasMostradas;
+
         if (preguntasMostradas >= 20)
         {
+            Console.WriteLine("Calificación obtenida: " + notaFinal);
             Console.WriteLine("Se han mostrado 20 preguntas. Fin del tipo test." + "\n");
         }
         else
         {
+            Console.WriteLine("Calficación obtenida: " + notaFinal);
             Console.WriteLine("No hay más preguntas disponibles. Fin del tipo test." + "\n");
         }
     }
-
-
 
     static void IntroducirNuevaPregunta()
     {
@@ -233,7 +250,6 @@ class Program
         }
 
         Console.WriteLine("==== Listado de preguntas ====");
-        Console.WriteLine("Lista de preguntas:");
 
         for (int i = 0; i < preguntas.Count; i++)
         {
@@ -276,6 +292,75 @@ class Program
             }
         }
     }
+
+    static async void PreguntarAChatGPT()
+    {
+        Console.Clear();
+        Console.WriteLine("==== Preguntar a ChatGPT ====");
+
+        Console.Write("Introduzca el enunciado de la pregunta: ");
+        string enunciado = Console.ReadLine();
+
+        List<string> opciones = new List<string>();
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.Write($"Introduzca la opción {i}: ");
+            string opcion = Console.ReadLine();
+            opciones.Add(opcion);
+        }
+
+        string preguntaCompleta = $"{enunciado}\n{string.Join("\n", opciones)}\nDime cuál es la respuesta correcta.";
+
+        string respuestaChat = await chatClient.Preguntar(preguntaCompleta);
+
+        Console.WriteLine("\nRespuesta de ChatGPT:");
+        Console.WriteLine(respuestaChat);
+        Console.WriteLine();
+    }
+}
+
+class ChatClient
+{
+    private const string OpenAIChatEndpoint = "https://api.openai.com/v1/chat/completions";
+    private const string OpenAIApiKey = "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";
+
+    public async Task<string> Preguntar(string pregunta)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {OpenAIApiKey}");
+
+            var datos = new
+            {
+                model = "gpt-3.5-turbo-0301", // Agregar el parámetro "model"
+                messages = new[]
+                {
+            new { role = "system", content = "You are ChatGPT, a large language model trained by OpenAI." },
+            new { role = "user", content = pregunta }
+        }
+            };
+
+            var respuesta = await httpClient.PostAsync(OpenAIChatEndpoint, new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(datos), Encoding.UTF8, "application/json"));
+
+            var respuestaJson = await respuesta.Content.ReadAsStringAsync();
+
+            dynamic respuestaModelo = Newtonsoft.Json.JsonConvert.DeserializeObject(respuestaJson);
+
+            if (respuestaModelo != null && respuestaModelo.choices != null && respuestaModelo.choices.Count > 0)
+            {
+                var respuestaChat = respuestaModelo.choices[0].message?.content;
+                return respuestaChat;
+            }
+            else
+            {
+                // Manejar el caso en el que la respuesta del servicio es nula o no tiene datos válidos
+                return "No se pudo obtener una respuesta válida del servicio ChatGPT.";
+            }
+        }
+
+
+    }
+
 }
 
 public class Pregunta
